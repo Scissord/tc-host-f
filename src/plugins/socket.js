feb0095f8a494db5ac5fe45292aea27a29ab101c
nurdaulet <scissxrd@gmail.com>
2024-12-27 11:23:40 +0500

    fix

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,9 +1,11 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+const URL = process.env.NODE_ENV === "production"
+  ? "https://api.talkcall-crm.com"
+  : "http://localhost:8080";
 
 export const socket = io(URL, {
   autoConnect: false,
   query: {
     user_id: null,
-  } 
+  }
 });
