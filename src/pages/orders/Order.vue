7479146277faf4bce231ccb645ea21c75a0f0456
nurdaulet <scissxrd@gmail.com>
2025-01-22 23:06:49 +0500

    fix

--- src/pages/orders/Order.vue
+++ src/pages/orders/Order.vue
@@ -236,9 +236,9 @@ const isProductInOrder = (productId) => {
 
           <div v-if="order?.state?.order?.doubles.length > 0" class="mt-3">
             <h1 class="font-bold text-sm">Дубли: {{ order?.state?.order?.doubles.length }} </h1>
-            <div class="flex items-center gap-2 mt-2">
+            <div class="flex flex-wrap items-center gap-2 mt-2">
               <p 
-                v-for="(double, index) in order?.state?.order?.doubles"
+                v-for="(double, index) in order?.state?.order?.doubles.length > 8 ? order?.state?.order?.doubles.slice(0, 8) : order?.state?.order?.doubles"
                 :key="index"
                 class="bg-blue-300 px-2 py-1 text-xs text-white cursor-pointer rounded-lg"
                 @click.left="() => handleDoublePress(double)"
