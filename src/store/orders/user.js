d0e3e0482d931c296ba3f25854e374d71e6c70dc
nurdaulet <scissxrd@gmail.com>
2025-01-28 11:14:37 +0500

    fix

--- src/store/orders/user.js
+++ src/store/orders/user.js
@@ -93,15 +93,15 @@ const useUserOrdersStore = defineStore('user_order', () => {
       .filter(order => order.is_checked)
       .map(order => order.id);
 
-    if (state.is_filtered) {
-      alert('Уберите фильтры!');
-      return;
-    };
+    // if (state.is_filtered) {
+    //   alert('Уберите фильтры!');
+    //   return;
+    // };
 
-    if (ids.length === 0) {
-      alert('Выберите заказы для обновления!');
-      return;
-    };
+    // if (ids.length === 0) {
+    //   alert('Выберите заказы для обновления!');
+    //   return;
+    // };
 
     const old_status = state.subStatuses.find((ss) => +ss.id === state.subStatus)
     const new_status = state.subStatuses.find((ss) => +ss.id === +val)
